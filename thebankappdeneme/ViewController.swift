7b6a5c1207922d0a8e8c3b71f1cf5aed3d635d44
Berke Kesgin <kesginberke9@gmail.com>
2024-06-18 13:58:17 +0300

    Initial Commit

---
+++ thebankappdeneme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  thebankappdeneme
+//
+//  Created by Berke Kesgin on 18.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
